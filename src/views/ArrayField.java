6665bc848b513faeb0739d87f4a24494f84c80dc
Moritz <Moritz@Moritz-PC.fritz.box>
2018-06-26 21:30:41 +0200

    Methode für Anzahl Buttons

--- src/views/ArrayField.java
+++ src/views/ArrayField.java
@@ -29,23 +29,21 @@ public class ArrayField {
 	}
 	public static void reset(JPanel panel) {
 		for(int i = 0; i<200; i++) {
-			for(int a = 60; a<640; a = a+28) {
-				for(int b = 0; b<280; b = b+28) {
-					panel.remove(list.get(i).getBlabel());
-					panel.remove(list.get(i).getButton());
-					panel.remove(list.get(i).getNlabel());
-					panel.repaint();
-				}
-			}
+			panel.remove(list.get(i).getBlabel());
+			panel.remove(list.get(i).getButton());
+			panel.remove(list.get(i).getNlabel());
+			panel.repaint();
 		}
 	}
 	public static void clear() {
 		list.clear();
 	}
+	
 	public static void neu(JPanel panel) {
-		for(int i = 0; i<200; i++) {
-			for(int a = 60; a<640; a = a+28) {
-				for(int b = 0; b<280; b = b+28) {
+		int i = 0;
+		for(int a = 60; a<640; a = a+28) {
+			for(int b = 0; b<280; b = b+28) {
+				if (i<200) {
 					list.add(new BTNfield(b,a,29,29));
 					panel.add(list.get(i).getButton());
 					if(ArrayElement.getBomb(i)==true) {
@@ -54,10 +52,11 @@ public class ArrayField {
 					if(ArrayElement.getFeld().get(i).getNumber()!=0) {
 						panel.add(list.get(i).getNlabel(i));
 					}
+					i++;
 				}
 			}
 		}
-	}
+	}	
 	public static void initNumber() {
 		for(int i=0; i<200; i++) {
 			Main.getJPanel().remove(list.get(i).getNlabel(i));
@@ -66,6 +65,22 @@ public class ArrayField {
 			if(ArrayElement.getFeld().get(i).getNumber()!=0) {
 				Main.getJPanel().add(list.get(i).getNlabel(i));
 			}
+		}
+	}
+	public static boolean getRemain() {
+		int i = 200-ArrayElement.getY();
+		int x = 0;
+		for(BTNfield y : list) {
+			if(!y.getButton().isVisible()) {
+				x++;
+			}
+		}
+		if(i-x == 0) {
+			x = 0;
+			return true;
+		} else {
+			x = 0;
+			return false;
 		}
 	}
 }
