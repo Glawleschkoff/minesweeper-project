b0e4c77d4fb497560a9e4037d2731b3ada040b51
Moritz <Moritz@Moritz-PC.fritz.box>
2018-06-26 21:37:08 +0200

    levelanpassung ingame möglich

---
+++ src/views/BTNlevel.java
@@ -0,0 +1,51 @@
+package views;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class BTNlevel {
+	private JButton btnLevel = new JButton("Novize");
+	private static int num = 1;
+	private String name;
+	public BTNlevel(JPanel panel) {
+		panel.add(btnLevel);
+		btnLevel.setBounds(117, 29, 164, 31);
+		btnLevel.setFont(new Font("Arial", Font.ITALIC, 20));
+		
+		btnLevel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(BTNfield.getStart()==false | BTNfield.getEnd()) {
+					high();
+					btnLevel.setText(name);
+					BTNnew.neu();
+				} else {
+					high();
+					btnLevel.setText(name);
+					BTNnew.setEnabled(true);
+				}
+				
+				
+			}
+		});
+	}
+	private void high() {
+		if(num==1) {
+			num++;
+			name = "Adept";
+		} else if(num==2) {
+			num++;
+			name = "Experte";
+		} else {
+			num = 1;
+			name = "Novize";
+		}
+	}
+	public static int getNum() {
+		return num;
+	}
+
+}
