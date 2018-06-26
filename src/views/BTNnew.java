3d543520d1d048f6babe505c93811e6fc8a6789c
Moritz <Moritz@Moritz-PC.fritz.box>
2018-06-26 21:33:54 +0200

    erweitert für "neu" durch level anpassung

--- src/views/BTNnew.java
+++ src/views/BTNnew.java
@@ -11,9 +11,11 @@ import resources.ImageIconScale;
 
 public class BTNnew {
 	private static JButton btnNew = new JButton("");
+	private static JPanel pane;
 	public BTNnew(JPanel panel) {
+		pane = panel;
 		panel.add(btnNew);
-		btnNew.setBounds(58, 0, 59, 59);
+		btnNew.setBounds(58, 1, 59, 59);
 		btnNew.setEnabled(false);
 		btnNew.setIcon(new ImageIconScale("neu",60).getImage());
 		
@@ -21,23 +23,26 @@ public class BTNnew {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
-				ArrayField.reset(panel);
-				ArrayField.clear();
-				ArrayElement.getFeld().clear();
-				Main.setFeld();
-				BTNfield.setR();
-				ArrayField.neu(panel);
-				btnNew.setEnabled(false);
-				BTNfield.setStart();
-				BTNfield.setEnd();
-				BTNflag.setEnabled(true);
-				LBLcounter.reset();
+				neu();
 			}			
 		});
 	}
 	public static void setEnabled(boolean b) {
 		btnNew.setEnabled(b);
 	}
-
+	public static void neu() {
+		ArrayField.reset(pane);
+		ArrayField.clear();
+		ArrayElement.getFeld().clear();
+		Main.setFeld();
+		BTNfield.setR();
+		ArrayField.neu(pane);
+		btnNew.setEnabled(false);
+		BTNfield.setStart();
+		BTNfield.setEnd();
+		BTNflag.setEnabled(true);
+		LBLcounter.reset();
+		BTNflag.setUnPressed();
+		LBLwin.remove();
+	}
 }
