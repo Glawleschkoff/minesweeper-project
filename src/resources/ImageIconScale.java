9bb73e7cd5a55a325e64d334503c6ea35a2eabcb
Moritz <Moritz@Moritz-PC.fritz.box>
2018-07-06 09:38:11 +0200

    idk

--- src/resources/ImageIconScale.java
+++ src/resources/ImageIconScale.java
@@ -10,15 +10,15 @@ public class ImageIconScale {
 	private ImageIcon ico; 
 	public ImageIconScale(String s, int x) {
 			if(s=="fahne") {
-				resource = ImageIconScale.class.getResource("./fahne.png");
+				resource = ImageIconScale.class.getResource("/fahne.png");
 				ico = new ImageIcon(resource);
 				ico.setImage(ico.getImage().getScaledInstance(x,x,Image.SCALE_DEFAULT));
 			} else if(s=="neu") {
-				resource = ImageIconScale.class.getResource("./neu.png");
+				resource = ImageIconScale.class.getResource("/neu.png");
 				ico = new ImageIcon(resource);
 				ico.setImage(ico.getImage().getScaledInstance(x,x,Image.SCALE_DEFAULT));
 			} else if (s=="bombe") {
-				resource = ImageIconScale.class.getResource("./bombe.png");
+				resource = ImageIconScale.class.getResource("/bombe.png");
 				ico = new ImageIcon(resource);
 				ico.setImage(ico.getImage());
 			}
